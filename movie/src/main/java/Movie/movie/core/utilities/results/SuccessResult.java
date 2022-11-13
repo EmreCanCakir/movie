3ed7b58513aa3023e8fdee2a9a785fbe2403a7d1
EmreCanCakir <emreecann28@gmail.com>
2022-11-13 08:45:48 +0300

    feat(result): add result structure to core/utilities/results
    add result structure to core/utilities/results
    
    #1

---
+++ movie/src/main/java/Movie/movie/core/utilities/results/SuccessResult.java
@@ -0,0 +1,12 @@
+package Movie.movie.core.utilities.results;
+
+public class SuccessResult extends Result {
+    public SuccessResult() {
+        super(true);
+    }
+
+    public SuccessResult(String message) {
+        super(true,message);
+    }
+
+}
