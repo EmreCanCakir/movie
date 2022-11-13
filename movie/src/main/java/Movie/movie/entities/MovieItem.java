cb16faf293af7186e25788d97014e6cc7f9cf7b4
EmreCanCakir <emreecann28@gmail.com>
2022-11-13 09:13:54 +0300

    feat(entities): add createdAt and lastModifiedAt
    add createdAt and lastModifiedAt
    
    #3

--- movie/src/main/java/Movie/movie/entities/MovieItem.java
+++ movie/src/main/java/Movie/movie/entities/MovieItem.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -13,6 +17,7 @@ import java.util.List;
 @Table(name = "movie_items")
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class MovieItem {
 
     @Id
@@ -47,4 +52,13 @@ public class MovieItem {
     @OneToMany(mappedBy = "movieItemId")
     @JsonBackReference
     private List<UserMovieItemComment> userMovieItemComments;
+
+    @Column(name = "created_at",nullable = false,updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at")
+    private LocalDateTime lastModifiedAt;
+
 }
